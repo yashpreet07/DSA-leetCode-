8a3c0e3fd86586d41e52611c2e9fb1a0ca708663
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-09-06 19:09:30 +0530

    Time: 23 ms (62.59%), Space: 45.2 MB (52.79%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int len = s.length();
+        
+        // Loop through each character of the string
+        for (int i = 0; i < len; i++) {
+            char currentChar = s.charAt(i);
+            
+            // Check if the first and last occurrence of the character are the same
+            if (s.indexOf(currentChar) == s.lastIndexOf(currentChar)) {
+                return i; // First unique character found, return its index
+            }
+        }
+        
+        return -1; // No unique character found
+    }
+}
+// TLE Solution
+// public int count(String str, char c) {
+// int count = 0;
+// for (char ch : str.toCharArray()) {
+// if (ch == c)
+// count++;
+// }
+// return count;
+// }
+// public int firstUniqChar(String s) {
+// int len = s.length()-1;
+// for (int i = 0; i <= len; i++) {
+// if (count(s, s.charAt(i)) == 1)
+// return i;
+// }
+// return -1;
