32ea647bf7a1f0394497172c95750a9f680a215c
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-09-12 21:48:01 +0530

    Time: 2 ms (99.13%), Space: 42.9 MB (70.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,54 @@
+// The Optimized code.
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        String rStr = removeNonAlphaNum(s);
-        int i = 0;
-        int j = rStr.length() - 1;
-        while (j >= i) {
-            if (rStr.charAt(i) != rStr.charAt(j))
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Move left pointer to the next alphanumeric character
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            // Move right pointer to the previous alphanumeric character
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            // Compare the characters (case-insensitive)
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
-            i++;
-            j--;
+            }
+            left++;
+            right--;
         }
         return true;
     }
-
- public String removeNonAlphaNum(String str) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            // Only add if the character is a letter or a digit
-            if (Character.isLetterOrDigit(ch)) {
-                sb.append(ch);
-            }
-        }
-        return sb.toString();
-    }
 }
+
+// Your code 
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         s = s.toLowerCase();
+//         String rStr = removeNonAlphaNum(s);
+//         int i = 0;
+//         int j = rStr.length() - 1;
+//         while (j >= i) {
+//             if (rStr.charAt(i) != rStr.charAt(j))
+//                 return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+
+//  public String removeNonAlphaNum(String str) {
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i < str.length(); i++) {
+//             char ch = str.charAt(i);
+//             // Only add if the character is a letter or a digit
+//             if (Character.isLetterOrDigit(ch)) {
+//                 sb.append(ch);
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
