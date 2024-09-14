1c578e91f7270c1be2ac33c1911827b9466964a2
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-09-14 23:11:04 +0530

    Time: 0 ms (100%), Space: 40.2 MB (83.26%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
@@ -20,22 +20,52 @@
 
 
 // Optimized Code
- class Solution {
+//  class Solution {
+//     public int divisorSubstrings(int num, int k) {
+//         int i = 0;
+//         int count = 0;
+//         String str = String.valueOf(num);
+//         int len = str.length();
+        
+//         while (i + k <= len) {
+//             int n = Integer.valueOf(str.substring(i, i + k));  // Get k-length substring as integer
+            
+//             // Check if n is a non-zero divisor of num
+//             if (n != 0 && num % n == 0) 
+//                 count++;
+            
+//             i++;  // Move the window
+//         }
+//         return count;  // Return the count of valid substrings
+//     }
+// }
+
+
+class Solution {
     public int divisorSubstrings(int num, int k) {
-        int i = 0;
-        int count = 0;
         String str = String.valueOf(num);
+        int count = 0;
         int len = str.length();
         
-        while (i + k <= len) {
-            int n = Integer.valueOf(str.substring(i, i + k));  // Get k-length substring as integer
-            
-            // Check if n is a non-zero divisor of num
-            if (n != 0 && num % n == 0) 
-                count++;
-            
-            i++;  // Move the window
+        // First k-length substring as an integer
+        int currentNum = Integer.parseInt(str.substring(0, k));
+        
+        // Check if the first k-length substring is a divisor
+        if (currentNum != 0 && num % currentNum == 0) {
+            count++;
         }
-        return count;  // Return the count of valid substrings
+        
+        // Sliding window through the rest of the string
+        for (int i = 1; i <= len - k; i++) {
+            // Calculate the next number in the window by adjusting the digits
+            currentNum = currentNum % (int) Math.pow(10, k - 1); // Remove the leading digit
+            currentNum = currentNum * 10 + (str.charAt(i + k - 1) - '0'); // Add the trailing digit
+            
+            if (currentNum != 0 && num % currentNum == 0) {
+                count++;
+            }
+        }
+        
+        return count;
     }
 }
