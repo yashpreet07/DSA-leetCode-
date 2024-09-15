706628a1b418cf1cf5e7b30f548100a122540415
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-09-15 22:19:42 +0530

    Time: 17 ms (22.71%), Space: 47.2 MB (98.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -52,26 +52,26 @@ To fix this, use a delimiter between the character frequencies to avoid ambiguit
 // }
 // More optimzed code
 // You can optimize the solution by avoiding frequency array manipulation and directly sorting the characters of each string. Sorting ensures that all anagrams produce the same sorted string, which can be used as a key in the map. This approach simplifies the logic and makes it faster in practice for moderate input sizes.
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map<String, List<String>> map = new HashMap<>();
         
-        for (String str : strs) {
-            // Sort the characters of the string
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String sortedStr = new String(chars);
+//         for (String str : strs) {
+//             // Sort the characters of the string
+//             char[] chars = str.toCharArray();
+//             Arrays.sort(chars);
+//             String sortedStr = new String(chars);
             
-            // Use sorted string as key
-            if (!map.containsKey(sortedStr)) {
-                map.put(sortedStr, new ArrayList<>());
-            }
-            map.get(sortedStr).add(str);
-        }
+//             // Use sorted string as key
+//             if (!map.containsKey(sortedStr)) {
+//                 map.put(sortedStr, new ArrayList<>());
+//             }
+//             map.get(sortedStr).add(str);
+//         }
         
-        return new ArrayList<>(map.values());
-    }
-}
+//         return new ArrayList<>(map.values());
+//     }
+// }
 /*Key Optimizations:
 
 	1.	Sorting Strings: By sorting each string, all anagrams will produce the same sorted string, allowing us to use it as a unique key in the map.
@@ -84,3 +84,29 @@ Time Complexity:
 
 This solution is generally more intuitive and has good performance for typical input si
 */
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String str : strs) {
+            // Frequency array for 26 lowercase letters
+            int[] freq = new int[26];
+            for (char ch : str.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+            
+            // Create a unique string key from the frequency array
+            StringBuilder key = new StringBuilder();
+            for (int count : freq) {
+                key.append(count).append('#'); // Use '#' as a delimiter between counts
+            }
+            
+            String keyStr = key.toString();
+            map.computeIfAbsent(keyStr, k -> new ArrayList<>()).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
