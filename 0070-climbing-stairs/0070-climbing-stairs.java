c5c9503a136cdb75e883b9de0d585e43badba1f6
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-09-22 19:24:43 +0530

    Time: 0 ms (100%), Space: 40.5 MB (19.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,37 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n == 0 || n == 1) {
+//             return 1;
+//         }
+        
+//         int[] dp = new int[n + 1];
+//         dp[0] = 1;
+//         dp[1] = 1;
+        
+//         for (int i = 2; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+        
+//         return dp[n];
+//     }
+// }
+
+
+// space optimization
 class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1) {
             return 1;
         }
         
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev1 = 1, prev2 = 1;
         
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
         }
         
-        return dp[n];
+        return prev1;
     }
 }
