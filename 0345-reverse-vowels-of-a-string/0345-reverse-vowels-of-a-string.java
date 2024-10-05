5666ef79732fe225d266cbb741d41c1003ad169a
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-10-05 19:15:06 +0530

    Time: 5 ms (38.3%), Space: 44.2 MB (99.18%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,29 +1,68 @@
+// class Solution {
+//     public boolean isVowel(char ltr)
+//     {   
+//         String vowel = "AEIOUaeiou";
+//         // return vowel.indexOf(ltr) !=-1 ? true : false; Instead of This Just do
+//         return vowel.indexOf(ltr) != -1;
+//     }
+//     public String reverseVowels(String s) {
+//         char []ch = s.toCharArray();
+//         int i = 0;
+//         int j = s.length()-1;
+//         while(i < j)
+//         {
+//             if(isVowel(ch[i]) && isVowel(ch[j]))
+//             {
+//                 char temp = ch[i];
+//                 ch[i] = ch[j];
+//                 ch[j] = temp;
+//                 i++;
+//                 j--;
+//             }
+//             if(!isVowel(ch[i]))
+//                 i++;
+//             if(!isVowel(ch[j]))
+//                 j--;
+//         }
+//       return String.valueOf(ch);
+//     }
+// }
+
 class Solution {
-    public boolean isVowel(char ltr)
-    {   
-        String vowel = "AEIOUaeiou";
-        // return vowel.indexOf(ltr) !=-1 ? true : false; Instead of This Just do
-        return vowel.indexOf(ltr) != -1;
+    private final HashSet<Character> vowels = new HashSet<>();
+    
+    public Solution() {
+        // Initialize the vowel set (for quick O(1) lookups)
+        vowels.add('A'); vowels.add('E'); vowels.add('I'); vowels.add('O'); vowels.add('U');
+        vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
     }
+
+    public boolean isVowel(char ltr) {
+        return vowels.contains(ltr);
+    }
+
     public String reverseVowels(String s) {
-        char []ch = s.toCharArray();
+        char[] ch = s.toCharArray();
         int i = 0;
-        int j = s.length()-1;
-        while(i < j)
-        {
-            if(isVowel(ch[i]) && isVowel(ch[j]))
-            {
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (isVowel(ch[i]) && isVowel(ch[j])) {
                 char temp = ch[i];
                 ch[i] = ch[j];
                 ch[j] = temp;
                 i++;
                 j--;
+            } else {
+                if (!isVowel(ch[i])) {
+                    i++;
+                }
+                if (!isVowel(ch[j])) {
+                    j--;
+                }
             }
-            if(!isVowel(ch[i]))
-                i++;
-            if(!isVowel(ch[j]))
-                j--;
         }
-      return String.valueOf(ch);
+        return String.valueOf(ch);
     }
 }
+
