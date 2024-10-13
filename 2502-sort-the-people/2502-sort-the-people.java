08c3af66e85e2c9641618e6733e1c12894a0e290
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-10-13 22:11:22 +0530

    Time: 7 ms (91.63%), Space: 45 MB (95.12%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,41 +1,21 @@
-// class Solution {
-//     public String[] sortPeople(String[] names, int[] heights) {
-//         Map<Integer,String> mp = new HashMap<>();
-//         int i = 0;
-//         while(i < names.length)
-//         {
-//             mp.put(heights[i],names[i]);
-//             i++;
-//         }
-//         Arrays.sort(heights);
-//         i = names.length - 1;
-//         int j = 0;
-//         while(i >= 0)
-//         {
-//             names[i] = mp.get(heights[j]);
-//             i--;
-//             j++;
-//         }
-//     return names;
-//     }
-// }
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        // Create an array of indices
-        Integer[] indices = new Integer[names.length];
-        for (int i = 0; i < names.length; i++) {
-            indices[i] = i; // Store the index of each person
+        Map<Integer,String> mp = new HashMap<>();
+        int i = 0;
+        while(i < names.length)
+        {
+            mp.put(heights[i],names[i]);
+            i++;
         }
-
-        // Sort the indices based on the heights in descending order
-        Arrays.sort(indices, (a, b) -> Integer.compare(heights[b], heights[a]));
-
-        // Create the result array and populate it with sorted names
-        String[] result = new String[names.length];
-        for (int i = 0; i < names.length; i++) {
-            result[i] = names[indices[i]];
+        Arrays.sort(heights);
+        i = names.length - 1;
+        int j = 0;
+        while(i >= 0)
+        {
+            names[i] = mp.get(heights[j]);
+            i--;
+            j++;
         }
-
-        return result;
+    return names;
     }
 }
