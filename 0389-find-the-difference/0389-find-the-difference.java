09b3b556bd90e203202bbd1b2883b62ad7167c0f
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-10-18 20:42:12 +0530

    Time: 2 ms (74.29%), Space: 41.9 MB (18.48%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,31 @@
+// Your Code missed If the character exists in both s and t, but t has more occurrences of that character than s, your current solution will not detect this. For example, in the case where s = "aabb" and t = "aabbb", the additional 'b' will be missed by the indexOf() check.
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         for(char ch : t.toCharArray())
+//         {
+//             if(s.indexOf(ch) == -1)
+//             return ch;
+//         }
+//     return 'a';}
+// }
+// count the frequency of each character in both strings and then compare those frequencies.
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] charCount = new int[26];  // Array to store character counts (for lowercase letters)
+
+        // Count characters in string `s`
+        for (char ch : s.toCharArray()) {
+            charCount[ch - 'a']++;
+        }
+
+        // Subtract counts based on string `t`, find the extra character
+        for (char ch : t.toCharArray()) {
+            charCount[ch - 'a']--;
+            if (charCount[ch - 'a'] < 0) {
+                return ch;  // This character has extra occurrences in `t`
+            }
+        }
+
+        return 'a';  // This line will never be reached, but is needed for compilation
+    }
+}
