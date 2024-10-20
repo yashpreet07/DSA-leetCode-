3bcaf8ca79b242b0bf64ad66b4d42df54860ae2d
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-10-20 19:32:24 +0530

    Time: 2 ms (98.98%), Space: 41.8 MB (57.43%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,3 +1,27 @@
+// Set<Integer> occurrenceSet = new HashSet<>(countMap.values());You didn't know you could do this!!!
+
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//         Map<Integer, Integer> countMap = new HashMap<>();
+        
+//         // Count occurrences of each element
+//         for (int x : arr) {
+//             countMap.put(x, countMap.getOrDefault(x, 0) + 1);
+//         }
+        
+//         // Check if the counts of occurrences are unique
+//         Set<Integer> occurrenceSet = new HashSet<>(countMap.values());
+        
+//         // If the size of occurrenceSet is the same as the countMap values, all occurrences are unique
+//         return occurrenceSet.size() == countMap.size();
+//     }
+// }
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer, Integer> countMap = new HashMap<>();
@@ -7,10 +31,18 @@ class Solution {
             countMap.put(x, countMap.getOrDefault(x, 0) + 1);
         }
         
-        // Check if the counts of occurrences are unique
-        Set<Integer> occurrenceSet = new HashSet<>(countMap.values());
+        // Use a single HashSet to check if occurrences are unique
+        Set<Integer> occurrenceSet = new HashSet<>();
         
-        // If the size of occurrenceSet is the same as the countMap values, all occurrences are unique
-        return occurrenceSet.size() == countMap.size();
+        // Iterate over the values in the map (which represent occurrences)
+        for (int count : countMap.values()) {
+            // If the set already contains this count, it's not unique
+            if (!occurrenceSet.add(count)) {
+                return false;
+            }
+        }
+        
+        // If all counts were unique, return true
+        return true;
     }
 }
