7af7170e3760c46f1b35f80b220e9ee91cd9d3cd
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2024-12-04 19:07:50 +0530

    Time: 5 ms (100%), Space: 45.8 MB (23.03%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
@@ -5,7 +5,7 @@
 //         int i = 0;
 //         int j = 0;
 //         int len = str1.length();
-//         int count = 0;
+        // int count = 0; When j pointer was there this really useless!!!
 //         int x = 1;
 //         while(i < len)
 //         {   if(str1.charAt(i) == 'z')
@@ -22,21 +22,39 @@
 //         return count == str2.length();
 //     }  
 // }
+// class Solution {
+//     public boolean canMakeSubsequence(String str1, String str2) {
+//         int i = 0;
+//         int j = 0;
+
+//         while (i < str1.length() && j < str2.length()) {
+//             char currentChar = str1.charAt(i);
+//             char nextChar = (currentChar == 'z') ? 'a' : (char) (currentChar + 1);
+            
+//             if (str2.charAt(j) == currentChar || str2.charAt(j) == nextChar) {
+//                 j++;
+//             }
+//             i++;
+//         }
+
+//         return j == str2.length();
+//     }
+// }
 class Solution {
     public boolean canMakeSubsequence(String str1, String str2) {
-        int i = 0;
-        int j = 0;
+        int ptr1 = 0, ptr2 = 0;
+        int n1 = str1.length(), n2 = str2.length();
 
-        while (i < str1.length() && j < str2.length()) {
-            char currentChar = str1.charAt(i);
-            char nextChar = (currentChar == 'z') ? 'a' : (char) (currentChar + 1);
-            
-            if (str2.charAt(j) == currentChar || str2.charAt(j) == nextChar) {
-                j++;
+        while (ptr1 < n1 && ptr2 < n2) {
+            char sch = str1.charAt(ptr1);
+            char tch = str2.charAt(ptr2);
+
+            if (sch == tch || sch + 1 == tch || sch - 25 == tch) {
+                ptr2++;
             }
-            i++;
+            ptr1++;
         }
 
-        return j == str2.length();
+        return ptr2 == n2;
     }
 }
