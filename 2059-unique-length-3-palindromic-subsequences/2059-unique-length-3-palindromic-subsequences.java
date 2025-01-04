2ae912ac4fa03b02e2a0680a844fb0a6437b7746
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2025-01-04 21:19:20 +0530

    Time: 171 ms (72.77%), Space: 45.5 MB (60.21%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -21,42 +21,64 @@
 //         return count;
 //     }
 // }
+// class Solution {
+//     public int countPalindromicSubsequence(String s) {
+//         int n = s.length();
+//         int count = 0;
+
+//         // Array to store the first and last occurrence of each character
+//         int[] first = new int[26];
+//         int[] last = new int[26];
+
+//         // Initialize first occurrence to -1
+//         for (int i = 0; i < 26; i++) {
+//             first[i] = -1;
+//         }
+
+//         // Populate first and last occurrence for each character
+//         for (int i = 0; i < n; i++) {
+//             int index = s.charAt(i) - 'a';
+//             if (first[index] == -1) {
+//                 first[index] = i;
+//             }
+//             last[index] = i;
+//         }
+
+//         // Count palindromic subsequences of length 3
+//         for (int i = 0; i < 26; i++) {
+//             if (first[i] != -1 && last[i] > first[i]) { // Valid range exists
+//                 HashSet<Character> uniqueChars = new HashSet<>();
+//                 // Collect unique characters between first[i] and last[i]
+//                 for (int j = first[i] + 1; j < last[i]; j++) {
+//                     uniqueChars.add(s.charAt(j));
+//                 }
+//                 // Add the count of unique characters
+//                 count += uniqueChars.size();
+//             }
+//         } 
+//         return count;
+//     }
+// }
 class Solution {
     public int countPalindromicSubsequence(String s) {
         int n = s.length();
         int count = 0;
 
-        // Array to store the first and last occurrence of each character
-        int[] first = new int[26];
-        int[] last = new int[26];
+        // Iterate over each character from 'a' to 'z' as potential first and last character
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int left = s.indexOf(ch);  // First occurrence
+            int right = s.lastIndexOf(ch); // Last occurrence
 
-        // Initialize first occurrence to -1
-        for (int i = 0; i < 26; i++) {
-            first[i] = -1;
-        }
-
-        // Populate first and last occurrence for each character
-        for (int i = 0; i < n; i++) {
-            int index = s.charAt(i) - 'a';
-            if (first[index] == -1) {
-                first[index] = i;
-            }
-            last[index] = i;
-        }
-
-        // Count palindromic subsequences of length 3
-        for (int i = 0; i < 26; i++) {
-            if (first[i] != -1 && last[i] > first[i]) { // Valid range exists
-                HashSet<Character> uniqueChars = new HashSet<>();
-                // Collect unique characters between first[i] and last[i]
-                for (int j = first[i] + 1; j < last[i]; j++) {
-                    uniqueChars.add(s.charAt(j));
+            if (right - left > 1) { // Valid range for middle characters
+                HashSet<Character> uniqueMiddle = new HashSet<>();
+                // Collect unique middle characters
+                for (int i = left + 1; i < right; i++) {
+                    uniqueMiddle.add(s.charAt(i));
                 }
-                // Add the count of unique characters
-                count += uniqueChars.size();
+                // Add count of unique middle characters
+                count += uniqueMiddle.size();
             }
         }
-
         return count;
     }
 }
