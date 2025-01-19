df8e703026cf15a4bd819d21c9fc551c1d3e858e
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2025-01-19 17:43:35 +0530

    Time: 0 ms (100%), Space: 55.4 MB (99.51%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    boolean check(int []arr,int m){
+        return (arr[m] > arr[m + 1]);
+    }
+    public int peakIndexInMountainArray(int[] arr) {
+        int lo = 0;
+        int hi = arr.length - 1;
+        int ans = 0;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if(check(arr,mid)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else
+                lo = mid + 1;
+        }
+        return ans;
+    }
+}
