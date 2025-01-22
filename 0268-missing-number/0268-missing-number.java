378fb6494fb45fc96a0ae6c4187af24e2c222355
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2025-01-22 10:33:16 +0530

    Time: 0 ms (100%), Space: 45.2 MB (68.84%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,35 @@
+// Your Solution 
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         int i = 0;
+//         for(;i < nums.length;i++){
+//             if(nums[i] != i)
+//                 return i;
+//         }
+//     return i == n ? i : -1;
+//     }
+// }
+// Taking sum approach
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n = nums.length;
+//         int sum = ((n) * (n + 1)) / 2;
+//         for(int i = 0; i < n;i++){
+//             sum -= nums[i];
+//         }
+//         return sum;
+//     }
+// }
+// XOR solution
 class Solution {
-    public int missingNumber(int[] nums) {
-        int n=nums.length;
-        Set<Integer> set=new HashSet<>();
-        for(int x : nums)
-            set.add(x);
-
-        for(int i=0;i<=n;i++)
-            if(!set.contains(i))
-            return i;
-    return -1;
+    public int missingNumber(int[] nums) { // xor
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
     }
 }
