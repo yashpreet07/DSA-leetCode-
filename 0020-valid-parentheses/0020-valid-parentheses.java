6910fa75261db3a2d569725602ec5dff8441658a
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2025-01-30 21:11:39 +0530

    Time: 2 ms (97.61%), Space: 41.8 MB (49.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else if(stack.isEmpty()){
+                return false;
+            }
+            else{
+                char top = stack.peek();
+                if( ch == ')' && top == '(' ||
+                    ch == '}' && top == '{' ||
+                    ch == ']' && top == '[' ){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
