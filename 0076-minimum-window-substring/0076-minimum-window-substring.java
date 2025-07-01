c3878a807065327bdaaca60663049319c85865b6
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2025-07-01 09:57:12 +0530

    Time: 17 ms (62.79%), Space: 44.8 MB (80.7%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Character,Integer> mp = new HashMap<>();
+        List<String> res = new ArrayList<>();
+        int n = t.length();
+        for(int i = 0;i < n;i++){
+            mp.put(t.charAt(i),mp.getOrDefault(t.charAt(i),0) + 1);
+        }
+        int i = 0 , j = 0 , m = s.length() , count = mp.size();
+        int minLen = Integer.MAX_VALUE;
+        int start = 0; // TO track the best window start index.
+        
+        if(m < n)   return "";
+
+        while(j < m){
+            char cj = s.charAt(j);
+            if(mp.containsKey(cj)){
+                mp.put(cj,mp.get(cj) - 1);
+                if(mp.get(cj) == 0)
+                count --;
+            }
+            while(count == 0){
+                if(j - i + 1 < minLen){
+                    minLen = j - i + 1;
+                    start = i;
+                }
+                char ci = s.charAt(i);
+                if(mp.containsKey(ci)){
+                    mp.put(ci,mp.get(ci) + 1);
+                    if(mp.get(ci) > 0)
+                    count ++;
+                }
+                i++;
+            }
+            j++;
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start,start + minLen);
+    }
+}
