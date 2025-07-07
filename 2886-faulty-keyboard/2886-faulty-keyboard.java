fb3e7b3929a0deef1719a89bd556c30dc6c9e43a
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2025-07-07 22:18:03 +0530

    Time: 3 ms (96.19%), Space: 44.9 MB (46.94%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == 'i') {
+                result.reverse();
+            } else {
+                result.append(ch);
+            }
+        }
+
+        return result.toString();
+    }
+}
