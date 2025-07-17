3034ab05f4a2d93e76e0918f0d0d4b43513003e0
yashpreet07 <146640693+yashpreet07@users.noreply.github.com>
2025-07-17 12:44:56 +0530

    Time: 1 ms (46.47%), Space: 45.4 MB (10.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,24 +1,34 @@
 class Solution {
     int dx[] = {-1,0,1,0};
     int dy[] = {0,-1,0,1};
-    void dfs(int i,int j,int [][]image,int color,int org_color){
-        for(int k = 0;k < 4;k++){
-            int ii = i + dx[k];
-            int jj = j + dy[k];
-            if(ii >= 0 && ii < image.length && jj >= 0 && jj < image[0].length){
-                if(image[ii][jj] == org_color){
-                    image[ii][jj] = color;
-                    dfs(ii,jj,image,color,org_color);
+    int row;
+    int col;
+    void bfs(int i,int j,int [][]image,int color,int org_color){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i,j});   
+        while(!q.isEmpty()){
+            int cell[] =  q.poll();
+            int ii = cell[0];
+            int jj = cell[1];
+
+            for(int k = 0;k < 4;k++){
+                int ni = ii + dx[k];
+                int nj = jj + dy[k];
+                if (ni >= 0 && ni < row && nj >= 0 && nj < col && image[ni][nj] == org_color){
+                    image[ni][nj] = color;
+                    q.offer(new int[]{ni,nj});
                 }
             }
-        }
+        }   
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int org_color = image[sr][sc];
+        row = image.length;
+        col = image[0].length;
         if(org_color == color)
             return image;
         image[sr][sc] = color;
-        dfs(sr,sc,image,color,org_color);
+        bfs(sr,sc,image,color,org_color);
         return image;
     }
 }
